8cbb607fa886db4ec2185d427284b8bebf3ffd85
Angelo Battaglia <angelobattaglia.math@gmail.com>
2024-04-02 13:57:30 +0200

    Update

--- app/populate_test.py
+++ app/populate_test.py
@@ -9,17 +9,17 @@ def populate_raccolte():
     cursor = conn.cursor()
 
     # Insert some records into the "utenti" table
-    cursor.execute('INSERT INTO utenti (id, nickname, email, password) VALUES (?, ?, ?)', ('utente001', 'nick001', 'email001', 'password001'))
-    cursor.execute('INSERT INTO utenti (id, nickname, email, password) VALUES (?, ?, ?)', ('utente002', 'nick002', 'email002', 'password002'))
-    cursor.execute('INSERT INTO utenti (id, nickname, email, password) VALUES (?, ?, ?)', ('utente003', 'nick003', 'email003', 'password003'))
-    cursor.execute('INSERT INTO utenti (id, nickname, email, password) VALUES (?, ?, ?)', ('utente004', 'nick004', 'email004', 'password004'))
-    cursor.execute('INSERT INTO utenti (id, nickname, email, password) VALUES (?, ?, ?)', ('utente005', 'nick005', 'email005', 'password005'))
-    cursor.execute('INSERT INTO utenti (id, nickname, email, password) VALUES (?, ?, ?)', ('utente006', 'nick006', 'email006', 'password006'))
-    cursor.execute('INSERT INTO utenti (id, nickname, email, password) VALUES (?, ?, ?)', ('utente007', 'nick007', 'email007', 'password007'))
-    cursor.execute('INSERT INTO utenti (id, nickname, email, password) VALUES (?, ?, ?)', ('utente008', 'nick008', 'email008', 'password008'))
-    cursor.execute('INSERT INTO utenti (id, nickname, email, password) VALUES (?, ?, ?)', ('utente009', 'nick009', 'email009', 'password009'))
-    cursor.execute('INSERT INTO utenti (id, nickname, email, password) VALUES (?, ?, ?)', ('utente010', 'nick010', 'email010', 'password0010'))
-    cursor.execute('INSERT INTO utenti (id, nickname, email, password) VALUES (?, ?, ?)', ('utente011', 'nick011', 'email011', 'password0011'))
+    cursor.execute('INSERT INTO utenti (id, nickname, email, password) VALUES (?, ?, ?)', ('utente001', 'nick001', 'email001@hello.com', 'password001'))
+    cursor.execute('INSERT INTO utenti (id, nickname, email, password) VALUES (?, ?, ?)', ('utente002', 'nick002', 'email002@hello.com', 'password002'))
+    cursor.execute('INSERT INTO utenti (id, nickname, email, password) VALUES (?, ?, ?)', ('utente003', 'nick003', 'email003@hello.com', 'password003'))
+    cursor.execute('INSERT INTO utenti (id, nickname, email, password) VALUES (?, ?, ?)', ('utente004', 'nick004', 'email004@hello.com', 'password004'))
+    cursor.execute('INSERT INTO utenti (id, nickname, email, password) VALUES (?, ?, ?)', ('utente005', 'nick005', 'email005@hello.com', 'password005'))
+    cursor.execute('INSERT INTO utenti (id, nickname, email, password) VALUES (?, ?, ?)', ('utente006', 'nick006', 'email006@hello.com', 'password006'))
+    cursor.execute('INSERT INTO utenti (id, nickname, email, password) VALUES (?, ?, ?)', ('utente007', 'nick007', 'email007@hello.com', 'password007'))
+    cursor.execute('INSERT INTO utenti (id, nickname, email, password) VALUES (?, ?, ?)', ('utente008', 'nick008', 'email008@hello.com', 'password008'))
+    cursor.execute('INSERT INTO utenti (id, nickname, email, password) VALUES (?, ?, ?)', ('utente009', 'nick009', 'email009@hello.com', 'password009'))
+    cursor.execute('INSERT INTO utenti (id, nickname, email, password) VALUES (?, ?, ?)', ('utente010', 'nick010', 'email010@hello.com', 'password0010'))
+    cursor.execute('INSERT INTO utenti (id, nickname, email, password) VALUES (?, ?, ?)', ('utente011', 'nick011', 'email011@hello.com', 'password0011'))
     # Insert some records into the "raccolte" table
     cursor.execute('INSERT INTO raccolte (nome, descrizione, data_creazione) VALUES (?, ?, ?)', ('Raccolta 1', 'Descrizione raccolta 1', '2021-01-01'))
     # Insert some records into the "donazioni" table
